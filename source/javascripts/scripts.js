92e26d3575058b07294f7e0d71022eedd0099b56
alauderdale <alauderdale@mac.com>
2014-01-14 01:29:28 -0700

    slider

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
 
+	  //slider
+	  var rsi = $('#slider-in-laptop').royalSlider({
+	    autoHeight: true,
+	    arrowsNav: false,
+	    fadeinLoadedSlide: true,
+	    controlNavigationSpacing: 0,
+	    controlNavigation: 'bullets',
+	    imageScaleMode: 'fill',
+	    imageAlignCenter:true,
+	    loop: true,
+	    loopRewind: true,
+	    numImagesToPreload: 20,
+	    keyboardNavEnabled: true,
+	    autoScaleSlider: true,  
+	    slidesSpacing: 0,
+	    globalCaption: true,
+	    autoScaleSliderWidth: 682,     
+	    autoScaleSliderHeight: 410
+	  }).data('royalSlider');
+	  $('#slider-next').click(function() {
+	    rsi.next();
+	  });
+	  $('#slider-prev').click(function() {
+	    rsi.prev();
+	  });
+
+
+
 	$('#find-out-how').bind('click', function(){
     pageScroller.next();
 	});
